248bd0830cf96080e7eb862a9bbcc8cf225f23cb
pochua117 <josh.flores.davila@gmail.com>
2025-07-31 08:32:14 -0600

    More debug

--- src/maps/CitadellePage.jsx
+++ src/maps/CitadellePage.jsx
@@ -7,6 +7,7 @@ const CitadellePage = () => {
 
   return (
     <section className="section-numbers">
+      {resultado1}
       <h2>BeamSmasher puzzle solver</h2>
       <div className="input-group">
         <InputBox setFunction={setX} value={X}/>
@@ -14,9 +15,10 @@ const CitadellePage = () => {
         <InputBox setFunction={setZ} value={Z}/>
       </div>
 
-      <div className="result">Result 1: {resultado1}</div>
-      <div className="result">Result 2: {resultado2}</div>
-      <h2 className="result">Result 3: {resultado3}</h2>
+      <div className="result" key={resultado1}>Result 1: {resultado1}</div>
+      <div className="result" key={resultado2}>Result 2: {resultado2}</div>
+      <h2 className="result"key={resultado3}>Result 3: {resultado3}</h2>
+      <h2 className="result">Result 3: {Math.random()}</h2>
       <script>
         {console.log("1: ",resultado1, typeof resultado1)}
         {console.log("2: ",resultado2, typeof resultado2)}

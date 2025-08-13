44bcb49369c5084f69baaba7995bed86c0c29457
pochua117 <josh.flores.davila@gmail.com>
2025-08-13 01:57:23 -0600

    Added Reckoning map with the basic folder puzzle

---
+++ src/maps/ReckoningPage.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import InputBox from "../components/InputBox.jsx";
+
+const ReckoningPage = () => {
+  const [items, setItems] = useState(
+    Array.from({ length: 4 }, (_, i) => ({
+      number: i + 1,
+      month: "" 
+    }))
+  );
+
+  const handleChange = (index, field, value) => {
+    setItems(prev =>
+      prev.map((item, i) =>
+        i === index ? { ...item, [field]: value } : item
+      )
+    );
+  };
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (!a.month) return 1;
+    if (!b.month) return -1;
+    console.log(a, b);
+    return (a.month) - (b.month);
+  });
+
+  const numerosOrdenados = sortedItems.map(item => item.number);
+
+  return (
+    <div className="section-numbers">
+    <h2>Object numbers</h2>
+    <div className="input-group">
+      {items.map((item, index) => (
+          <InputBox
+            key={index}
+            value={item.number}
+            setFunction={(val) => handleChange(index, "number", Number(val))}
+          />
+      ))}
+    </div>
+    <h2>Object months</h2>
+    <div className="input-group">
+      {items.map((item, index) => (
+          <InputBox
+            key={index}
+            value={item.month}
+            setFunction={(val) => handleChange(index, "month", Number(val))}
+          />
+      ))}
+    </div>
+      <p>{numerosOrdenados.join(", ")}</p>
+    </div>
+  );
+};
+
+export default ReckoningPage;

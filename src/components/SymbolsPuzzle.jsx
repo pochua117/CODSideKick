1aa1c5328fb916d4314f8cf1f81474abe567c3ca
pochua117 <josh.flores.davila@gmail.com>
2025-08-23 00:25:22 -0600

    Added subtitle style

--- src/components/SymbolsPuzzle.jsx
+++ src/components/SymbolsPuzzle.jsx
@@ -11,7 +11,7 @@ const SymbolsPuzzle = ({ name, description, length, resultlenght, imagesDir, ima
   return (
     <div className="container">
       <h2 className="symbols-title">{name}</h2>
-      <h3 className="symbols-title">{description}</h3>
+      <h3 className="symbols-subtitle">{description}</h3>
       <SymbolsGallery setClickSymbolId={setClickSymbolId} imagesDir={imagesDir} imageExtension={imageExtension} length={length} />
       <SymbolSlots clickSymbolId={clickSymbolId} imagesDir={imagesDir} length={resultlenght} imageExtension={imageExtension} />
     </div>
--- src/components/maps_components/CitadellePage.jsx
+++ src/components/maps_components/CitadellePage.jsx
@@ -80,7 +80,7 @@ const CitadellePage = () => {
         <div className={activeTab === "puzzle4" ? "puzzle-visible" : "puzzle-hidden"}>
           <div>
             <h2 className="symbols-title">Raven Sword Puzzle Solver</h2>
-            <h3 className="symbols-title">
+            <h3 className="symbols-subtitle">
               Solve the Raven sword puzzle by finding the fossil and selecting its corresponding zodiac symbol and element. On this page, you can see each fossil with its symbols for reference.
             </h3>
             <SymbolsGallery 

c2ae505e14df8d42d15ba73f0125715268d90b87
pochua117 <josh.flores.davila@gmail.com>
2025-08-22 21:59:47 -0600

    Added "Easter Egg"

--- src/pages/maps/terminus.jsx
+++ src/pages/maps/terminus.jsx
@@ -7,10 +7,10 @@ export default function Terminus() {
   return (
     <>
       <Head>
-        <title>Terminus Zombies Puzzle Solver - BO6 Beam Smasher Calculator</title>
+        <title>Terminus Easter Egg - Zombies Puzzle Solver - BO6 Beam Smasher Calculator</title>
         <meta
           name="description"
-          content="Solve the Terminus Beam Smahser puzzle in Call of Duty Black Ops 6 Zombies. Use this Beam Smasher calculator: enter the three symbols and instantly get the correct result."
+          content="Solve the Terminus Easter Egg Beam Smahser puzzle in Call of Duty Black Ops 6 Zombies. Use this Beam Smasher calculator: enter the three symbols and instantly get the correct result."
         />
       </Head>
       <MapPageContent mapName="Terminus">

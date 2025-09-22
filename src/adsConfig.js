deba10c739a4e1e840ddc2d49cc2e4b2bbb285fd
pochua117 <josh.flores.davila@gmail.com>
2025-09-22 16:48:40 -0600

    added bottom ad to all maps

--- src/adsConfig.js
+++ src/adsConfig.js
@@ -2,10 +2,12 @@ export const adsConfig = {
   "Terminus": {
     left: "5858146858",
     right: "1572800370",
+    down: "4717293290",
   },
   "Citadelle des Morts": {
     left: "2558578980",
     right: "1245497313",
+    down: "6408760862",
   },
   "test": {
     left: "1368522914",
@@ -15,5 +17,6 @@ export const adsConfig = {
   "Reckoning": {
     left: "1368522914",
     right: "6816005309",
+    down: "3747471336",
   },
 };
